5e4d4e35138053f91b3889664eda2397cf300124
Catur Setyono <catursetyono542@gmail.com>
2026-08-02 21:24:10 +0700

    refactor(packages/web/lib/wagmi.ts): auto update

--- packages/web/lib/wagmi.ts
+++ packages/web/lib/wagmi.ts
@@ -14,13 +14,17 @@ export const wagmiConfig = getDefaultConfig({
 });
 
 export const PRIVATE_OTC_ADDRESS =
-  (process.env.NEXT_PUBLIC_PRIVATE_OTC_ADDRESS as `0x${string}`) ?? "0x0";
+  (process.env.NEXT_PUBLIC_PRIVATE_OTC_ADDRESS as `0x${string}`) ||
+  "0xE45cf4bcbF060Ada54188203684a18db1A3aeFC2";
 
 export const CUSDC_ADDRESS =
-  (process.env.NEXT_PUBLIC_CUSDC_ADDRESS as `0x${string}`) ?? "0x0";
+  (process.env.NEXT_PUBLIC_CUSDC_ADDRESS as `0x${string}`) ||
+  "0x5269822fc0127A9531c6B63dF0227E463e8DC411";
 
 export const CETH_ADDRESS =
-  (process.env.NEXT_PUBLIC_CETH_ADDRESS as `0x${string}`) ?? "0x0";
+  (process.env.NEXT_PUBLIC_CETH_ADDRESS as `0x${string}`) ||
+  "0x2774C01B0De131aEEFb7F668ee8B6f326B66D276";
 
 export const TRADI_NOX_RECEIPT_ADDRESS =
-  (process.env.NEXT_PUBLIC_TRADI_NOX_RECEIPT_ADDRESS as `0x${string}`) ?? "0x0";
+  (process.env.NEXT_PUBLIC_TRADI_NOX_RECEIPT_ADDRESS as `0x${string}`) ||
+  "0x1fCFb4176e71e7298011a55B57F033E0c8881634";
